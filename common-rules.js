c9d2cccc935117ae1492df2080999082ed12dcbb
Mike Bell <mikel3377@gmail.com>
2021-07-13 20:53:44 -0400

    Allow void as statement so it can be used to ignore promises
    This is the suggested way to explicitly ignore promises returns.
    See https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-floating-promises.md#ignorevoid

--- common-rules.js
+++ common-rules.js
@@ -58,7 +58,7 @@
       'no-useless-concat': error(),
       'no-useless-escape': error(),
       'no-useless-return': error(),
-      'no-void': error(),
+      'no-void': error({allowAsStatement: true}),
       'no-with': error(),
       'radix': error(),
       'vars-on-top': error(),
